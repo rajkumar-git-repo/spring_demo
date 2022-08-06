ae136e4816d9ed2bde1fbd6a1ad6197174c4f6a8
RAJ <rajkumar20192@gmail.com>
2022-08-07 02:17:13 +0530

    add manager file

---
+++ src/main/java/com/demo/entity/Manager.java
@@ -0,0 +1,33 @@
+package com.demo.entity;
+
+public class Manager {
+
+	private int id;
+	private String name;
+	private String salary;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSalary() {
+		return salary;
+	}
+
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+
+}
