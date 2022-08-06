76c4e423b8717433196d7325b7df42ee7ccd203c
rajkumar-git-repo <rajkumar20192@gmail.com>
2022-08-06 16:05:33 +0530

    add print method

---
+++ src/main/java/com/demo/main/SpringDemoApplication.java
@@ -0,0 +1,16 @@
+package com.demo.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class SpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoApplication.class, args);
+		System.out.println("***********SPRING DEMO*****************");
+	}
+
+}
